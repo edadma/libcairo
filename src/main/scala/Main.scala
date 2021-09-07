ecc42b44f1a073073eb44c3458a8e2cd81cdf171
Edward A Maxedon, Sr <edadma@gmail.com>
2021-09-07 17:05:41 -0400

    code second example

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,7 @@ import io.github.edadma.libcairo._
 
 object Main extends App {
 
-  val surface = image_surface_create(Format.ARGB32, 120, 120);
+  val surface = image_surface_create(Format.ARGB32, 120, 120)
   val cr      = surface.create
 
   cr.scale(120, 120)
@@ -27,9 +27,47 @@ object Main extends App {
   cr.set_source_rgba(0, 0, 1, 0.40)
   cr.fill()
 
-  surface.write_to_png("image.png")
+  surface.write_to_png("image1.png")
 
   cr.destroy()
   surface.destroy()
 
 }
+
+//object Main extends App {
+//
+//  val surface = image_surface_create(Format.ARGB32, 120, 120)
+//  val cr      = surface.create
+//
+//  /* Examples are in 1.0 x 1.0 coordinate space */
+//  cr.scale(120, 120)
+//
+//  val radpat = pattern_create_radial(0.25, 0.25, 0.1, 0.5, 0.5, 0.5)
+//
+//  radpat.add_color_stop_rgb(0, 1.0, 0.8, 0.8)
+//  radpat.add_color_stop_rgb(1, 0.9, 0.0, 0.0)
+//
+//  for (i <- 1 to 9; j <- 1 to 9)
+//    cr.rectangle(i / 10.0 - 0.04, j / 10.0 - 0.04, 0.08, 0.08)
+//
+//  cr.set_source(radpat)
+//  cr.fill()
+//
+//  val linpat = pattern_create_linear(0.25, 0.35, 0.75, 0.65)
+//
+//  linpat.add_color_stop_rgba(0.00, 1, 1, 1, 0)
+//  linpat.add_color_stop_rgba(0.25, 0, 1, 0, 0.5)
+//  linpat.add_color_stop_rgba(0.50, 1, 1, 1, 0)
+//  linpat.add_color_stop_rgba(0.75, 0, 0, 1, 0.5)
+//  linpat.add_color_stop_rgba(1.00, 1, 1, 1, 0)
+//
+//  cr.rectangle(0.0, 0.0, 1, 1)
+//  cr.set_source(linpat)
+//  cr.fill()
+//
+//  surface.write_to_png("image2.png")
+//
+//  cr.destroy()
+//  surface.destroy()
+//
+//}
