f597537e026fa2d0509b73a731db8a0323ebb6f9
Edward A Maxedon <edadma@gmail.com>
2023-01-03 14:28:54 -0500

    update

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,8 @@
 //import io.github.edadma.libcairo._
 //
-//object Main extends App {
-//
+//@main def run(): Unit =
 //  val surface = imageSurfaceCreate(Format.ARGB32, 120, 120)
-//  val cr      = surface.create
+//  val cr = surface.create
 //
 //  cr.scale(120, 120)
 //
@@ -31,15 +30,12 @@
 //
 //  cr.destroy()
 //  surface.destroy()
-//
-//}
 
 //import io.github.edadma.libcairo._
 //
-//object Main extends App {
-//
+//@main def run(): Unit =
 //  val surface = imageSurfaceCreate(Format.ARGB32, 120, 120)
-//  val cr      = surface.create
+//  val cr = surface.create
 //
 //  /* Examples are in 1.0 x 1.0 coordinate space */
 //  cr.scale(120, 120)
@@ -49,8 +45,7 @@
 //  radpat.addColorStopRGB(0, 1.0, 0.8, 0.8)
 //  radpat.addColorStopRGB(1, 0.9, 0.0, 0.0)
 //
-//  for (i <- 1 to 9 j <- 1 to 9)
-//    cr.rectangle(i / 10.0 - 0.04, j / 10.0 - 0.04, 0.08, 0.08)
+//  for i <- 1 to 9; j <- 1 to 9 do cr.rectangle(i / 10.0 - 0.04, j / 10.0 - 0.04, 0.08, 0.08)
 //
 //  cr.setSource(radpat)
 //  cr.fill()
@@ -71,17 +66,14 @@
 //
 //  cr.destroy()
 //  surface.destroy()
-//
-//}
 
 //import math._
 //
 //import io.github.edadma.libcairo._
 //
-//object Main extends App {
-//
+//@main def run(): Unit =
 //  val surface = imageSurfaceCreate(Format.ARGB32, 120, 120)
-//  val cr      = surface.create
+//  val cr = surface.create
 //
 //  cr.scale(120, 120)
 //
@@ -100,79 +92,77 @@
 //  surface.writeToPNG("path-close.png")
 //  cr.destroy()
 //  surface.destroy()
+
+//import math._
 //
-//}
-
-import math._
-
-import io.github.edadma.libcairo._
-
-@main def run(): Unit =
-  /* Prepare drawing area */
-  val surface = imageSurfaceCreate(Format.ARGB32, 240, 240)
-  val cr = surface.create
-
-  /* Example is in 26.0 x 1.0 coordinate space */
-  cr.scale(240, 240)
-  cr.setFontSize(0.5)
-
-  /* Drawing code goes here */
-  cr.setSourceRGB(0.0, 0.0, 0.0)
-  cr.selectFontFace("Georgia", FontSlant.NORMAL, FontWeight.BOLD)
-
-  val fe = cr.fontExtents
-  val (ux, uy) = cr.deviceToUserDistance(1, 1)
-  val px = max(ux, uy)
-  val text = "joy"
-  val te = cr.textExtents(text)
-  val x = 0.5 - te.xBearing - te.width / 2
-  val y = 0.5 - fe.descent + fe.height / 2
-
-  /* baseline, descent, ascent, height */
-  cr.setLineWidth(4 * px)
-  cr.setDash(Seq(9 * px), 0)
-  cr.setSourceRGBA(0, 0.6, 0, 0.5)
-  cr.moveTo(x + te.xBearing, y)
-  cr.relLineTo(te.width, 0)
-  cr.moveTo(x + te.xBearing, y + fe.descent)
-  cr.relLineTo(te.width, 0)
-  cr.moveTo(x + te.xBearing, y - fe.ascent)
-  cr.relLineTo(te.width, 0)
-  cr.moveTo(x + te.xBearing, y - fe.height)
-  cr.relLineTo(te.width, 0)
-  cr.stroke()
-
-  /* extents: width & height */
-  cr.setSourceRGBA(0, 0, 0.75, 0.5)
-  cr.setLineWidth(px)
-  cr.setDash(Seq(3 * px), 0)
-  cr.rectangle(x + te.xBearing, y + te.yBearing, te.width, te.height)
-  cr.stroke()
-
-  /* text */
-  cr.moveTo(x, y)
-  cr.setSourceRGB(0, 0, 0)
-  cr.showText(text)
-
-  /* bearing */
-  cr.setDash(Nil, 0)
-  cr.setLineWidth(2 * px)
-  cr.setSourceRGBA(0, 0, 0.75, 0.5)
-  cr.moveTo(x, y)
-  cr.relLineTo(te.xBearing, te.yBearing)
-  cr.stroke()
-
-  /* text's advance */
-  cr.setSourceRGBA(0, 0, 0.75, 0.5)
-  cr.arc(x + te.xAdvance, y + te.yAdvance, 5 * px, 0, 2 * Pi)
-  cr.fill()
-
-  /* reference point */
-  cr.arc(x, y, 5 * px, 0, 2 * Pi)
-  cr.setSourceRGBA(0.75, 0, 0, 0.5)
-  cr.fill()
-
-  /* Write output and clean up */
-  surface.writeToPNG("textextents.png")
-  cr.destroy()
-  surface.destroy()
+//import io.github.edadma.libcairo._
+//
+//@main def run(): Unit =
+//  /* Prepare drawing area */
+//  val surface = imageSurfaceCreate(Format.ARGB32, 240, 240)
+//  val cr = surface.create
+//
+//  /* Example is in 26.0 x 1.0 coordinate space */
+//  cr.scale(240, 240)
+//  cr.setFontSize(0.5)
+//
+//  /* Drawing code goes here */
+//  cr.setSourceRGB(0.0, 0.0, 0.0)
+//  cr.selectFontFace("Georgia", FontSlant.NORMAL, FontWeight.BOLD)
+//
+//  val fe = cr.fontExtents
+//  val (ux, uy) = cr.deviceToUserDistance(1, 1)
+//  val px = max(ux, uy)
+//  val text = "joy"
+//  val te = cr.textExtents(text)
+//  val x = 0.5 - te.xBearing - te.width / 2
+//  val y = 0.5 - fe.descent + fe.height / 2
+//
+//  /* baseline, descent, ascent, height */
+//  cr.setLineWidth(4 * px)
+//  cr.setDash(Seq(9 * px), 0)
+//  cr.setSourceRGBA(0, 0.6, 0, 0.5)
+//  cr.moveTo(x + te.xBearing, y)
+//  cr.relLineTo(te.width, 0)
+//  cr.moveTo(x + te.xBearing, y + fe.descent)
+//  cr.relLineTo(te.width, 0)
+//  cr.moveTo(x + te.xBearing, y - fe.ascent)
+//  cr.relLineTo(te.width, 0)
+//  cr.moveTo(x + te.xBearing, y - fe.height)
+//  cr.relLineTo(te.width, 0)
+//  cr.stroke()
+//
+//  /* extents: width & height */
+//  cr.setSourceRGBA(0, 0, 0.75, 0.5)
+//  cr.setLineWidth(px)
+//  cr.setDash(Seq(3 * px), 0)
+//  cr.rectangle(x + te.xBearing, y + te.yBearing, te.width, te.height)
+//  cr.stroke()
+//
+//  /* text */
+//  cr.moveTo(x, y)
+//  cr.setSourceRGB(0, 0, 0)
+//  cr.showText(text)
+//
+//  /* bearing */
+//  cr.setDash(Nil, 0)
+//  cr.setLineWidth(2 * px)
+//  cr.setSourceRGBA(0, 0, 0.75, 0.5)
+//  cr.moveTo(x, y)
+//  cr.relLineTo(te.xBearing, te.yBearing)
+//  cr.stroke()
+//
+//  /* text's advance */
+//  cr.setSourceRGBA(0, 0, 0.75, 0.5)
+//  cr.arc(x + te.xAdvance, y + te.yAdvance, 5 * px, 0, 2 * Pi)
+//  cr.fill()
+//
+//  /* reference point */
+//  cr.arc(x, y, 5 * px, 0, 2 * Pi)
+//  cr.setSourceRGBA(0.75, 0, 0, 0.5)
+//  cr.fill()
+//
+//  /* Write output and clean up */
+//  surface.writeToPNG("textextents.png")
+//  cr.destroy()
+//  surface.destroy()
